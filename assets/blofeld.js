9aa2ad747a4f0b6207682736a544b354d05f8422
Dave Hayes <dwhayes@gmail.com>
2010-01-23 00:15:18 -0600

    Lots of new stuff... cover art, transcoding, search, etc

--- assets/blofeld.js
+++ assets/blofeld.js
@@ -1,24 +1,28 @@
 var loadingImage = "<div class='scrollingContainer'><img src='assets/images/loading.gif' alt='loading...' class='loading'></div>";
 
-
 var playSong = function (song) {
-    audioElement.setAttribute('src', 'get_song?songid=' + song);
-    audioElement.setAttribute('controls', 'true');
-    audioElement.load();
-    audioElement.addEventListener("canplay", function () {
-        audioElement.play()
-    }, false);
+    $("#player").setFile('get_song?format=mp3&songid=' + song, 'get_song?format=ogg&songid=' + song).play();
+    $('#cover_art').html('<a href="get_cover?size='+ Math.floor(document.body.clientHeight * 0.75) + '&songid=' + song + '"><img src="get_cover?size=32&songid=' + song + '" width="32" height="32"></a>')
+    $('#cover_art > a').lightBox({imageLoading:'assets/images/loading.gif', songid:song});
+    $('#np_title').html($('#' + song + ' .title').html())
+    $('#np_artist').html($('#' + song + ' .artist').html())
+    $('#np_album').html($('#' + song + ' .album').html())
 }
 
-var listArtists = function () {
+var listArtists = function (query) {
+    options = {'output': 'html'}
+    if (query) {
+        options['query'] = query
+    }
     $("#artistsContainer").html(loadingImage).load(
         'list_artists',
-        {'output': 'html'}, 
+        options, 
         function () {
             $("#artists").tablesorter({
                 sortForce: [[0,0]],
                 sortList: [[0,0]]
             }); 
+            $('.artist').unbind('click');
             $('.artist').click(function (event) {
                 if (!event.ctrlKey) {
                     $('.artist.selected').removeClass('selected')
@@ -32,15 +36,18 @@ var listArtists = function () {
                 $('.artist.selected').each(function () {
                     selectedArtists.push($(this).attr('id'))
                 })
-                listAlbums(selectedArtists)
-                listSongs(selectedArtists)
+                listAlbums(selectedArtists, $('#query').val())
+                listSongs(selectedArtists, null, $('#query').val())
             })
         }
     )
 }
 
-var listAlbums = function (artists) {
+var listAlbums = function (artists, query) {
     options = {'output': 'html'}
+    if (query) {
+        options['query'] = query
+    }
     if (artists) {
         options['artists'] = artists.join(',')
     }
@@ -52,6 +59,7 @@ var listAlbums = function (artists) {
                 sortForce: [[0,0]],
                 sortList: [[0,0]]
             })
+            $('.album').unbind('click');
             $('.album').click(function (event) {
                 if (!event.ctrlKey) {
                     $('.album.selected').removeClass('selected')
@@ -65,14 +73,17 @@ var listAlbums = function (artists) {
                 $('.album.selected').each(function () {
                     selectedAlbums.push($(this).attr('id'))
                 })
-                listSongs(artists, selectedAlbums)
+                listSongs(artists, selectedAlbums, $('#query').val())
             })
         }
     )
 }
 
-var listSongs = function (artists, albums) {
+var listSongs = function (artists, albums, query) {
     options = {'output': 'html'}
+    if (query) {
+        options['query'] = query
+    }
     if (artists) {
         options['artists'] = artists.join(',')
     }
@@ -87,9 +98,12 @@ var listSongs = function (artists, albums) {
                 sortForce: [[3,0], [4,0], [0,0]],
                 sortList: [[3,0], [4,0], [0,0]]
             }); 
+            $("#songs tr:odd").addClass('tinted')
+            $('.song').unbind('dblclick');
             $('.song').dblclick(function () {
                 playSong($(this).attr('id'))
             })
+            $('.song').unbind('click');
             $('.song').click(function (event) {
                 if (!event.ctrlKey) {
                     $('.song.selected').removeClass('selected')
@@ -104,6 +118,23 @@ var listSongs = function (artists, albums) {
     )
 }
 
+var setupPlayer = function () {
+    $("#player").jPlayer({
+        ready: function () {
+            return
+        },
+        swfPath: "assets"
+    })
+    .jPlayerId("play", "play")
+    .jPlayerId("pause", "pause")
+    .onProgressChange( function(loadPercent, playedPercentRelative, playedPercentAbsolute, playedTime, totalTime) {
+        return
+    })
+    .onSoundComplete( function() {
+        return
+    })
+}
+
 var disableSelection = function (target){
     if (typeof target.onselectstart!="undefined") //IE route
         target.onselectstart=function(){return false}
@@ -114,23 +145,18 @@ var disableSelection = function (target){
     target.style.cursor = "default"
 }
 
-var setupPlayer = function () {
-    audioElement = document.createElement('audio');
-    $('#media-playback-pause').hide();
-    audioElement.addEventListener('playing', function () {
-        $('#media-playback-start').hide();
-        $('#media-playback-pause').show();
-    }, false)
-    audioElement.addEventListener('pause', function () {
-        $('#media-playback-pause').hide();
-        $('#media-playback-start').show();
-    }, false)
-    $('#media-playback-pause').click(function () {
-        audioElement.pause();
-    })
-    $('#media-playback-start').click(function () {
-        audioElement.play();
-    })
+var resizeInterface = function () {
+    if (window.innerHeight) {
+        $('#browser').height(window.innerHeight - 86);
+    } else if (document.all) {
+        $('#browser').height(document.body.clientHeight - 86);
+    }
+}
+
+var find = function () {
+    listSongs(null, null, $('#query').val())
+    listAlbums(null, $('#query').val())
+    listArtists($('#query').val())
 }
 
 $(document).ready(function() {
@@ -139,11 +165,13 @@ $(document).ready(function() {
     })
     setupPlayer();
     disableSelection(document.getElementById("browser"))
-    
     listArtists();
     listAlbums();
-
-    
+    resizeInterface();
+    $('#clear_search').click(function () {
+        $('#query').val('');
+        find();
+    })
 })
 
 
--- blofeld.py
+++ blofeld.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
-
 import os
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+from urlparse import urlparse
+import urllib
 import urllib2
 import hashlib
 import json
 from Cheetah.Template import Template
 import cherrypy
+from cherrypy.lib.static import serve_file
+from PIL import Image
+import subprocess
+
 try:
     from xml.etree.cElementTree import ElementTree
 except:
@@ -17,10 +20,11 @@ except:
 
 import mimetypes 
 mimetypes.init() 
-
+program_dir = os.path.dirname(os.path.abspath(__file__))
 database = "/home/dhayes/.local/share/rhythmbox/rhythmdb.xml"
+#database = "/home/dhayes/Desktop/rhythmdb.xml"
 
-class GetMusicList:
+class Blofeld:
     def __init__(self):
         self._path = os.path.dirname(os.path.abspath(__file__))
         print "Loading Rhythmbox database...",
@@ -31,9 +35,12 @@ class GetMusicList:
         self.artists = {}
         self.albums = {}
         self.relationships = {}
+        self.load_rhythmbox_db()
+
+    def load_rhythmbox_db(self):
         for entry in self._entries:
             if entry.attrib['type'] == "song":
-                location_hash = hashlib.sha1(entry.find('location').text).hexdigest()
+                location_hash = hashlib.sha1(entry.find('location').text.encode("utf-8")).hexdigest()
                 self.songs[location_hash] = {}
                 for element in entry:
                     tag = element.tag
@@ -43,12 +50,12 @@ class GetMusicList:
                         self.songs[location_hash][tag] = ''
                     finally:
                         if tag == "artist":
-                            artist_hash = hashlib.sha1(self.songs[location_hash][tag]).hexdigest()
+                            artist_hash = hashlib.sha1(self.songs[location_hash][tag].encode("utf-8")).hexdigest()
                             self.songs[location_hash]['artist_hash'] = artist_hash
                             if artist_hash not in self.artists:
                                 self.artists[artist_hash] = self.songs[location_hash][tag]
                         if tag == "album":
-                            album_hash = hashlib.sha1(self.songs[location_hash][tag]).hexdigest()
+                            album_hash = hashlib.sha1(self.songs[location_hash][tag].encode("utf-8")).hexdigest()
                             self.songs[location_hash]['album_hash'] = album_hash
                             if album_hash not in self.albums:
                                 self.albums[album_hash] = self.songs[location_hash][tag]
@@ -68,14 +75,25 @@ class GetMusicList:
         return template.respond()
     index.exposed = True
 
-    def list_albums(self, artists=None, search_term=None, output='json'):
-        result = None
+    def list_albums(self, artists=None, query=None, output='json'):
+        result = {}
         if artists:
             artists = artists.split(',')
-            result = {}
             for artist in artists:
                 for album in self.relationships[artist]:
                     result[album] = self.albums[album]
+        if query:
+            filter_result = {}
+            if not result:
+                result = self.albums
+            for song in self.songs:
+                if query.lower() in self.songs[song]['artist'].lower() and self.songs[song]['album_hash'] in result and self.songs[song]['album_hash'] not in filter_result:
+                    filter_result[self.songs[song]['album_hash']] = self.songs[song]['album']
+                elif query.lower() in self.songs[song]['album'].lower() and self.songs[song]['album_hash'] in result and self.songs[song]['album_hash'] not in filter_result:
+                    filter_result[self.songs[song]['album_hash']] = self.songs[song]['album']
+                elif query.lower() in self.songs[song]['title'].lower() and self.songs[song]['album_hash'] in result and self.songs[song]['album_hash'] not in filter_result:
+                    filter_result[self.songs[song]['album_hash']] = self.songs[song]['album']
+            result = filter_result
         if output == 'json':
             if result:
                 return str(result)
@@ -86,21 +104,37 @@ class GetMusicList:
         else: 
             template.albums = self.albums
         return template.respond()
+    list_albums._cp_config = {'response.stream': True}
     list_albums.exposed = True
 
-    def list_artists(self, search_term=None,  output='json'):
+    def list_artists(self, query=None,  output='json'):
+        result = {}
+        if query:
+            for song in self.songs:
+                if query.lower() in self.songs[song]['artist'].lower() and self.songs[song]['artist_hash'] not in result:
+                    result[self.songs[song]['artist_hash']] = self.songs[song]['artist']
+                elif query.lower() in self.songs[song]['album'].lower() and self.songs[song]['artist_hash'] not in result:
+                    result[self.songs[song]['artist_hash']] = self.songs[song]['artist']
+                elif query.lower() in self.songs[song]['title'].lower() and self.songs[song]['artist_hash'] not in result:
+                    result[self.songs[song]['artist_hash']] = self.songs[song]['artist']
         if output == 'json':
-            return str(self.artists)
+            if result:
+                return str(result)
+            else:
+                return str(self.artists)
         template = Template(file=os.path.join(self._path, 'templates/list_artists.tmpl'))
-        template.artists = self.artists
+        if result:
+            template.artists = result
+        else:
+            template.artists = self.artists
         return template.respond()
+    list_artists._cp_config = {'response.stream': True}
     list_artists.exposed = True
 
-    def list_songs(self, artists=None, albums=None, search_term=None,  output='json'):
-        result = None
+    def list_songs(self, artists=None, albums=None, query=None,  output='json'):
+        result = {}
         if albums and not artists:
             albums = albums.split(',')
-            result = {}
             for album in albums:
                 for artist in self.relationships:
                     if album in self.relationships[artist]:
@@ -108,53 +142,125 @@ class GetMusicList:
                             result[song] = self.songs[song]
         if artists and not albums:
             artists = artists.split(',')
-            result = {}
             for artist in artists:
                 for album in self.relationships[artist]:
                     for song in self.relationships[artist][album]:
                         result[song] = self.songs[song]
         if (artists != None) and (albums != None):
-            result = {}
             artists = artists.split(',')
             albums = albums.split(',')
-            for artist in artists:
-                for album in albums:
-                    for song in self.relationships[artist][album]:
-                        result[song] = self.songs[song]
+            for album in albums:
+                for artist in artists:
+                    try:
+                        for song in self.relationships[artist][album]:
+                            result[song] = self.songs[song]
+                    except:
+                        pass
+        if query:
+            filter_result = {}
+            if not result:
+                result = self.songs
+            for song in result:
+                if query.lower() in result[song]['artist'].lower() and song not in filter_result:
+                    filter_result[song] = result[song]
+                elif query.lower() in result[song]['album'].lower() and song not in filter_result:
+                    filter_result[song] = result[song]
+                elif query.lower() in result[song]['title'].lower() and song not in filter_result:
+                    filter_result[song] = result[song]
+            result = filter_result
         if output =='json':
-            if result != None:
-                return str(result)
-            return "too many songs"
+            return str(result)
         template = Template(file=os.path.join(self._path, 'templates/list_songs.tmpl'))
-        if result != None:
-            template.songs = result
-        else: 
-            return "too many songs"
+        template.songs = result
         return template.respond()
+    list_songs._cp_config = {'response.stream': True}
     list_songs.exposed = True
 
-    def get_song(self, songid=None):
+    def get_song(self, songid=None, download=False, format=None):
+        if songid not in self.songs:
+            return "False"
+        uri = self.songs[songid]['location']
+        path = urllib.url2pathname(urlparse(uri).path)
+        song = urllib2.urlopen(uri)
+        print song.info()['Content-Type']
+        if download:
+            return serve_file(path, song.info()['Content-Type'], os.path.split(path)[1])
+        if format != None:
+            return self.transcode(path, song, format)
+        else:
+            cherrypy.response.headers['Content-Type'] = song.info()['Content-Type']
+            return song.read()
+        return False
+    get_song.exposed = True
+    get_song._cp_config = {'response.stream': True}
+
+    def transcode(self, path, song, format):
+        if format == 'mp3':
+            cherrypy.response.headers['Content-Type'] = 'audio/mpeg'
+            if song.info()['Content-Type'] == 'audio/mpeg':
+                return song.read()
+            ffmpeg = subprocess.Popen(['/usr/bin/ffmpeg', '-i', path, '-f', 'mp3', '-ab', '160k', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=-1)
+            def stream():
+                try:
+                    print "\nreading initial data"
+                    yield ffmpeg.stdout.read(327680)
+                    print "\nreading remaining data"
+                    yield ffmpeg.stdout.read()
+                except: 
+                    print "\nTranscoding stopped or finished"
+                    return
+            return stream();
+        if format == 'ogg':
+            cherrypy.response.headers['Content-Type'] = 'audio/ogg'
+            if song.info()['Content-Type'] == 'audio/ogg':
+                return song.read()
+            ffmpeg = subprocess.Popen(['/usr/bin/ffmpeg', '-i', path, '-f', 'ogg', '-acodec', 'vorbis', '-aq', '40', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=-1)
+            return ffmpeg.communicate()[0]
+
+    def get_cover(self, songid=None, size='original', download=False):
         if songid not in self.songs:
             return "Can't find that song."
-        f = urllib2.urlopen(self.songs[songid]['location'])
-        cherrypy.response.headers['Content-Type'] = f.info()['Content-Type']
-        read_data = f.read()
-        return read_data
-    get_song.exposed = True
+        uri = self.songs[songid]['location']
+        path = os.path.split(urllib.url2pathname(urlparse(uri).path))[0]
+        cover = 'Cover.jpg'
+        artwork = urllib2.urlopen('file://' + urllib.pathname2url(os.path.join(path, cover)))
+        if download:
+            return serve_file(os.path.join(path, cover), artwork.info()['Content-Type'], cover)
+        if size != 'original':
+            size = int(size)
+            img_path = os.path.join(program_dir + '/cache', songid + '-' + str(size) + '.jpg')
+            try:
+                artwork = urllib2.urlopen('file://' + urllib.pathname2url(img_path))
+            except:
+                image = Image.open(os.path.join(path, cover))
+                if image.size[0] > size or image.size[1] > size:
+                    wpercent = (size/float(image.size[0]))
+                    hsize = int((float(image.size[1])*float(wpercent)))
+                    image = image.resize((size,hsize), Image.ANTIALIAS)
+                    image.save(os.path.join(urllib.pathname2url(img_path)))
+                    artwork = urllib2.urlopen('file://' + urllib.pathname2url(img_path))
+                else:
+                    artwork = urllib2.urlopen('file://' + urllib.pathname2url(os.path.join(path, cover)))
+        else:
+            artwork = urllib2.urlopen('file://' + urllib.pathname2url(os.path.join(path, cover)))
+        cherrypy.response.headers['Content-Type'] = artwork.info()['Content-Type']
+        return artwork.read()
+        #return serve_file(os.path.join(path, cover), f.info()['Content-Type'], cover)
+    get_cover.exposed = True
 
 if __name__ == '__main__':
-    current_dir = os.path.dirname(os.path.abspath(__file__))
+    
     cherrypy.config.update({
         'server.socket_host': '0.0.0.0',
         'tools.encode.on':True, 
-        'tools.encode.encoding':'utf8'
+        'tools.encode.encoding':'utf-8'
         })
 
     conf = {
         '/assets': {
             'tools.staticdir.on': True,
-            'tools.staticdir.dir': os.path.join(current_dir, 'assets')}
+            'tools.staticdir.dir': os.path.join(program_dir, 'assets')}
         }
 
-    cherrypy.quickstart(GetMusicList(), '/', config=conf)
+    cherrypy.quickstart(Blofeld(), '/', config=conf)
 
