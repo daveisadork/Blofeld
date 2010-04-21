4fc59c465f53095b766df0b1318a30a140c29778
Dave Hayes <dwhayes@gmail.com>
2010-04-21 12:42:26 -0500

    Stops Rhythmbox plugin from blocking UI all the time

--- extra/blofeld-rhythmbox/__init__.py
+++ extra/blofeld-rhythmbox/__init__.py
@@ -1,5 +1,6 @@
 import rhythmdb, rb
 import gobject, gtk
+import gnome, gconf
 import urllib2
 import cjson as json
 import datetime
@@ -10,7 +11,6 @@ class Blofeld(rb.Plugin):
         rb.Plugin.__init__(self)
 
     def activate(self, shell):
-        print "activating sample python plugin"
         self.db = shell.get_property("db")
         self.entry_type = self.db.entry_register_type("BlofeldEntryType")
         model = self.db.query_model_new_empty()
@@ -51,6 +51,9 @@ class BlofeldSource(rb.BrowserSource):
     def __init__(self):
         rb.BrowserSource.__init__(self, name=_("Blofeld"))
         self.__activated = False
+        self.__updating = False
+        self.__load_total_size = 0
+        self.url = "http://192.168.111.65/blofeld"
 
     def do_impl_activate(self):
         if not self.__activated:
@@ -59,13 +62,28 @@ class BlofeldSource(rb.BrowserSource):
             self.__entry_type = self.get_property('entry-type')
             self.__activated = True
         rb.BrowserSource.do_impl_activate (self)
-        song_list = urllib2.urlopen('http://192.168.111.65/blofeld/list_songs?list_all=true')
-        songs = json.decode(song_list.read())
-        trackurl = 'http://192.168.111.65/blofeld/get_song?songid='
-        for song in songs:
-            entry = self.__db.entry_lookup_by_location (trackurl + song['id'])
+        loader = rb.Loader()
+        self.__updating = True
+        loader.get_url(self.url + '/list_songs?list_all=true', self.parse_song_list)
+
+
+    def parse_song_list(self, song_list):
+        self.songs = json.decode(song_list)
+        self.__load_total_size = len(self.songs)
+        self.__load_current_size = len(self.songs)
+        self.trackurl = self.url + '/get_song?songid='
+        self.finished = False
+        gobject.idle_add(self.add_song)
+
+    def add_song(self):
+        gtk.gdk.threads_enter()
+        for x in range(100):
+            if not self.songs:
+                break
+            song = self.songs.pop(0)
+            entry = self.__db.entry_lookup_by_location (self.trackurl + song['id'])
             if entry == None:
-                entry = self.__db.entry_new(self.__entry_type, trackurl + song['id'])
+                entry = self.__db.entry_new(self.__entry_type, self.trackurl + song['id'])
             self.__db.set(entry, rhythmdb.PROP_ARTIST, song['artist'])
             self.__db.set(entry, rhythmdb.PROP_ALBUM, song['album'])
             self.__db.set(entry, rhythmdb.PROP_TITLE, song['title'])
@@ -89,6 +107,13 @@ class BlofeldSource(rb.BrowserSource):
             except:
                 pass
             self.__db.commit()
+            x += 1
+        self.__load_current_size = len(self.songs)
+        gtk.gdk.threads_leave()
+        if not self.songs:
+            self.__updating = False
+            return False
+        return True
 
     def playing_entry_changed (self, entry):
         if not self.__db or not entry:
@@ -102,5 +127,27 @@ class BlofeldSource(rb.BrowserSource):
         self.__db.emit_entry_extra_metadata_notify (entry, 'rb:coverArt-uri', url)
         return False
 
+    def do_impl_get_status(self):
+        if self.__updating:
+            if self.__load_total_size > 0:
+                progress = min (float(self.__load_total_size - self.__load_current_size) / self.__load_total_size, 1.0)
+            else:
+                progress = -1.0
+            return (_("Loading Blofeld library"), None, progress)
+        else:
+            qm = self.get_property("query-model")
+            return (qm.compute_status_normal("%d song", "%d songs"), None, None)
+
+	def do_set_property(self, property, value):
+		if property.name == 'plugin':
+			self.__plugin = value
+		else:
+			raise AttributeError, 'unknown property %s' % property.name
+
+	def do_impl_get_browser_key (self):
+		return "/apps/rhythmbox/plugins/blofeld-rhythmbox/show_browser"
+
+	def do_impl_get_paned_key (self):
+		return "/apps/rhythmbox/plugins/blofeld-rhythmbox/paned_position"
 
 gobject.type_register(BlofeldSource)
